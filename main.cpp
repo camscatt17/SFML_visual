fa1681fde6d1af23bb800d6fd260d0f47847cb64
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-02-05 18:45:53 -0300

    Lógica para movimento linear do personagem

--- main.cpp
+++ main.cpp
@@ -1,21 +1,39 @@
+//megaman spritesheet with 85/70 pixels
+
 #include "SFML/Graphics.hpp"
 
 int main() {
 
 	//Passo básico para renderizar uma janela
-	sf::RenderWindow window(sf::VideoMode(256, 256), "first image");
+	sf::RenderWindow window(sf::VideoMode(640, 480), "megaman runnig");
 	sf::Event event;
 
 	sf::Texture texture;
-	texture.loadFromFile("courseSFML/resources/red.png");
+	texture.loadFromFile("courseSFML/resources/mega.png");
 
-	sf::Sprite sprite(texture, sf::IntRect(0, 0, 32, 32));
+	//Cria-se um objeto do tipo Rect que representará o tamanho do sprite
+	sf::IntRect rect(0, 0, 85, 70);
+
+	sf::Sprite sprite(texture, rect);
+
+	sf::Clock clock;
 
 
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::EventType::Closed)//Enquanto não fecharmos uma janela, mantemos ela ativa
 				window.close();
+		}
+
+		if (clock.getElapsedTime().asSeconds() > 0.4f) {
+			if (rect.left == 255)
+				rect.left = 0;
+			else
+				rect.left += 85;
+
+			//É necessário atualizar o spite a cada passagem do loop
+			sprite.setTextureRect(rect);
+			clock.restart();
 		}
 
 		window.clear();
